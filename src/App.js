8dda75423564542f3b043ae0d8bcba79b7d4759f
dolphinedaisy <dhwanij2@gmail.com>
2020-08-11 17:50:09 +0530

    30. Understanding JSX

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // this is jsx or js not HTML, behind the scenes it will compile into line written below in the comment
+    // see the className attr, in HTML we write just class not className
+    return (
+        <div className="App">
+            <h1>Hi, I am REACT App</h1>
+        </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work ?'));
 }
 
 export default App;
