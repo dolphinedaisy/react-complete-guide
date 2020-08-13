4ea4a6e025787ead7c9d5dd08adae5a36a155a4d
dolphinedaisy <dhwanij2@gmail.com>
2020-08-13 18:19:01 +0530

    56. Outputting Lists

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-// word after import can be anything, but best practice is using component name starting with capital character
-// any tag starts with lowercase is used for built in tags like <div>, <h1>
-// so custom component tag start with capital letter is best practice
-// and can be easily differentiated
 import Person from "./Person/Person";
 
 class App extends Component {
@@ -26,44 +22,6 @@ class App extends Component {
         showPersons: false,
     }
 
-    switchNameHandler = (newName, newAge) => {
-        this.setState({
-            persons: [
-                {
-                    name: newName,
-                    age: newAge
-                },
-                {
-                    name: 'Vaidehi',
-                    age: 25
-                },
-                {
-                    name: 'Saloni',
-                    age: 20
-                }
-            ],
-        });
-    }
-
-    nameChangedHandler = (event) => {
-        this.setState({
-            persons: [
-                {
-                    name: 'Dhwani',
-                    age: 10
-                },
-                {
-                    name: 'Vaidehi',
-                    age: 25
-                },
-                {
-                    name: event.target.value,
-                    age: 20
-                }
-            ],
-        });
-    }
-
     togglePersonListHandler = () => {
         // const fuckYou = this.state.showPersons;
         this.setState({ showPersons: !(this.state.showPersons) });
@@ -71,12 +29,26 @@ class App extends Component {
 
     render() {
 
+        let persons = null;
+
+        if(this.state.showPersons) {
+            persons = (
+                <div>
+                    {
+                        this.state.persons.map((person, index) => {
+                            return <Person key={index} name={person.name} age={person.age}></Person>
+                        })
+                    }
+                </div>
+            );
+        }
+
         // whatever style written here inside is scoped to the component, it is NOT GLOBAL.
         // restriction are there, like you can not add :hover lie pseudo class
         const btnStyleGreen = {
             'color': '#fff',
-            'background-color': '#28a745',
-            'border-color': '#28a745',
+            'backgroundColor': '#28a745',
+            'borderColor': '#28a745',
         }
 
         return (
@@ -87,19 +59,8 @@ class App extends Component {
                 <button style={ btnStyleGreen } className="btn mb-3"
                         onClick={() => this.togglePersonListHandler()}>Toggle</button>
 
-                {
-                    this.state.showPersons ?
-                    <div>
-                        <Person clickLabel={this.switchNameHandler.bind(this, 'Aparna', 19)}
-                                name={this.state.persons[0].name}
-                                age={this.state.persons[0].age}></Person>
-                        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Dancing and
-                            painting</Person>
-                        <Person changed={this.nameChangedHandler}
-                                name={this.state.persons[2].name}
-                                age={this.state.persons[2].age}></Person>
-                    </div> : null
-                }
+                { persons }
+
             </div>
         );
     }
