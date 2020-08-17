0b2f13e9e16eebaa31a60e8866dff65b90051ee7
dolphinedaisy <dhwanij2@gmail.com>
2020-08-17 18:48:07 +0530

    104. Using React.Fragment

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './Person.css';
 import ReactAux from '../../hoc/ReactAux';
 
@@ -22,14 +22,16 @@ class Person extends Component {
 
         // option - 2
         return (
-            <ReactAux>
+            // <React.Fragment> or
+            <Fragment>
                 <hr/>
                 <button data-delete="qwerty" onClick={ this.props.click } className={'btn btn-danger'}>Delete</button>
                 <p>My name is <b>{ this.props.name }</b>  and I am <b> { this.props.age } </b> years old</p>
                 <p>My hobbies are { this.props.children ? this.props.children : 'Nothing' }</p>
                 <input type="text" onChange={this.props.changed} value={ this.props.name }/>
                 <hr/>
-            </ReactAux>
+            </Fragment>
+            // </React.Fragment>
         );
     }
 }
