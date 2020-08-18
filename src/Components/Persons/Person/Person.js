ab9888f3df3bffe7ff1cc949025ad0b69e1bbe97
dolphinedaisy <dhwanij2@gmail.com>
2020-08-18 11:27:43 +0530

    107. Passing Unknown Props

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import './Person.css';
+import withClass from "../../../hoc/withClass";
+import ReactAux from "../../../hoc/ReactAux";
 
 class Person extends Component {
 
     render() {
         console.log('Person.js render()');
         return (
-            <div className={'Person my-3'}>
+            <ReactAux>
                 <button data-delete="qwerty" onClick={ this.props.click } className={'btn btn-danger'}>Delete</button>
                 <p>My name is <b>{ this.props.name }</b>  and I am <b> { this.props.age } </b> years old</p>
                 <p>My hobbies are { this.props.children ? this.props.children : 'Nothing' }</p>
                 <input type="text" onChange={this.props.changed} value={ this.props.name }/>
-            </div>
+            </ReactAux>
         );
     }
 }
-export default Person;
+export default withClass(Person, 'Person my-3');
