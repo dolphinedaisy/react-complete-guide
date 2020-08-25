0a7433a48d4d2652d3875cffb17371cc25aeba74
dolphinedaisy <dhwanij2@gmail.com>
2020-08-25 18:17:57 +0530

    164. Transforming Data 163. Rendering Fetched Data to the Screen 162. Creating a Http Request to GET Data

--- src/Containers/Blog/Blog.js
+++ src/Containers/Blog/Blog.js
@@ -3,15 +3,37 @@ import classes from './Blog.module.css';
 import Post from "../../Components/Post/Post";
 import FullPost from "../../Components/FullPost/FullPost";
 import NewPost from "../../Components/NewPost/NewPost";
+import axios from "axios";
 
 class Blog extends Component {
+
+    state = {
+        posts: []
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => {
+                const posts = response.data.slice(0,3);
+                const updatedPosts = posts.map(rd => {
+                    return {
+                        ...rd,
+                        author: 'Dhwani'
+                    };
+                })
+                this.setState({posts: updatedPosts});
+            });
+    }
+
     render () {
+        let posts = this.state.posts.map((post, i) => {
+            return <Post key={post.id} author={post.author} title={post.title.substring(1, 4)} />;
+        });
+
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
