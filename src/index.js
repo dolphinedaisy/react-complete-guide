861be9c0ec67e05de90541def0492e60780f00f6
dolphinedaisy <dhwanij2@gmail.com>
2020-08-25 21:04:17 +0530

    171. Adding Interceptors to Execute Code Globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Containers/App';
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
+
+axios.interceptors.request.use((request) => {
+    console.log('interceptors Request : ', request);
+    // you must return the request otherwise you are blocking the request
+    return request
+}, error => {
+    // this catch block can be used for local error like no internet connection
+    // those error that blocks the request can be catch and managed here
+    console.log('interceptors Error : ', error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use((response) => {
+    console.log('interceptors Response : ', response);
+    // must return response from here
+    // otherwise you won't receive it in then block
+    return response;
+}, error => {
+    console.log('interceptors Error : ', error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render(
   //  https://stackoverflow.com/a/61091205
